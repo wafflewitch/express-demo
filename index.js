d688dadfe05411275284ad75a88bc65c0c35eee8
wafflewitch <jessicapruitt717@gmail.com>
2020-05-12 12:28:57 -0500

    Add comments. Add public directory to house index.html and images. Configure express to serve static files in public directory.

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+// set up requirements and assign variables
+const path = require('path');
 const express = require('express');
 
+// invoke express and assign variable
 const app = express();
 
+// tell express to make a request
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
+  // tell express where to find desired file to serve
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// tell express to serve static files and where to find them
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set up port and assign variable
 const port = process.env.PORT || 5000;
 
+// tell express which port to listen to
 app.listen(port, () => console.log(`Server running on ${port}`));
